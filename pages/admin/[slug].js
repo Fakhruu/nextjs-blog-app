40e3942a03bea1cee140d6ef36e35a1539d8445c
Fakhru Razi <me@fakhru.my>
2022-03-12 15:49:44 +0800

    Pages

---
+++ pages/admin/[slug].js
@@ -0,0 +1,7 @@
+export default function AdminSlugPage({}){
+    return (
+        <main>
+            <h1>Admin Edit Page</h1>
+        </main>
+    )
+}
---
+++ pages/admin/[username]/[slug].js
@@ -0,0 +1,5 @@
+export default function UserPostPage({}){
+    return (
+        <main> </main>
+    )
+}
